33a72d43637d7db4d58797881a2df988cb5cbef7
Adrian <adriancaef@gmail.com>
2021-10-05 18:56:50 +0200

    subo calculator-html

---
+++ calculator-html/Display.js
@@ -0,0 +1,64 @@
+// Clase cpara el display (pantalla) de la calculadora
+class Display {
+
+    constructor(displayValorAnterior, displayValorActual) {
+        this.displayValorActual = displayValorActual;
+        this.displayValorAnterior = displayValorAnterior;
+        this.calculador = new Calculadora();
+        this.tipoOperacion = undefined;
+        this.valorActual = '';
+        this.valorAnterior = '';
+        this.signos = {
+            sumar: '+',
+            dividir: '/',
+            multiplicar: 'x',
+            restar: '-', 
+        }
+    }
+
+    // Borra la ultima posición del string con los números de valorActual
+    borrar() {
+        this.valorActual = this.valorActual.toString().slice(0,-1);
+        this.imprimirValores();
+    }
+
+    // Borra toda la pantalla
+    borrarTodo() {
+        this.valorActual = '';
+        this.valorAnterior = '';
+        this.tipoOperacion = undefined;
+        this.imprimirValores();
+    }
+
+    // Dependiendo del tipo de operación, llama al método calcular y muestras los calculos por el display
+    computar(tipo) {
+        this.tipoOperacion !== 'igual' && this.calcular();
+        this.tipoOperacion = tipo;
+        this.valorAnterior = this.valorActual || this.valorAnterior;
+        this.valorActual = '';
+        this.imprimirValores();
+    }
+
+    // Agrega un número a valorActual. Si es una coma(,) solo agrega una
+    agregarNumero(numero) {
+        if(numero === '.' && this.valorActual.includes('.')) return
+        this.valorActual = this.valorActual.toString() + numero.toString();
+        this.imprimirValores();
+    }
+
+    // Imprime los valores en el display
+    imprimirValores() {
+        this.displayValorActual.textContent = this.valorActual;
+        this.displayValorAnterior.textContent = `${this.valorAnterior} ${this.signos[this.tipoOperacion] || ''}`;
+    }
+
+    // Hace los calculos dependiendo del tipo de operación
+    calcular() {
+        const valorAnterior = parseFloat(this.valorAnterior);
+        const valorActual = parseFloat(this.valorActual);
+
+        if( isNaN(valorActual)  || isNaN(valorAnterior) ) return
+        this.valorActual = this.calculador[this.tipoOperacion](valorAnterior, valorActual);
+    }
+
+}
